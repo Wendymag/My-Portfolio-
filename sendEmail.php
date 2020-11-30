90638163d73f754494f3556889fc652e92026586
Wendymag <71039462+Wendymag@users.noreply.github.com>
2020-11-30 11:48:19 +0200

    Add files via upload

---
+++ sendEmail.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style>
+  /*style*/
+  * {
+  box-sizing: border-box;
+}
+
+/* Style inputs */
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type=submit] {
+  background-color: #09e0c4;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+/* Style the container/contact section */
+.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 10px;
+}
+
+/* Create two columns that float next to eachother */
+.column {
+  float: left;
+  width: 50%;
+  margin-top: 6px;
+  padding: 20px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .column, input[type=submit] {
+    width: 100%;
+    margin-top: 0;
+  }
+}
+  
+ /*lusanda  wendy magobiane*/ 
+* {
+  box-sizing: border-box;
+}
+
+/* Style the body */
+body { background-color:lightgray;
+  font-family: Arial;
+  margin: 0;
+}
+
+/* Header/logo Title */
+.header {
+  padding: 60px;
+  text-align: center;
+  background: #1abc9c;
+  color: white;
+}
+
+/* Style the top navigation bar */
+.navbar {
+  display: flex;
+  background-color: #333;
+}
+
+/* Style the navigation bar links */
+.navbar a {
+  color: white;
+  padding: 14px 20px;
+  text-decoration: none;
+  text-align: center;
+}
+
+/* Change color on hover */
+.navbar a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Column container */
+.row {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+/* Create two unequal columns that sits next to each other */
+/* Sidebar/left column */
+.side {
+  flex: 30%;
+  background-color:lightblue;
+  padding: 20px;
+}
+
+/* Main column */
+.main {
+  flex: 70%;
+  background-image:url("apple.jpg");
+  background-color: white;
+  padding: 20px;
+}
+
+/* Fake image, just for this example */
+.fakeimg {
+  background-color: #aaa;
+  width: 20%;
+  padding: 20px;
+}
+
+
+/* Responsive layout - when the screen is less than 700px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 700px) {
+  .row, .navbar {
+    flex-direction: column;
+  }
+}
+</style>
+</head>
+<body>
+
+<!-- Note -->
+<div style="background:lightskyblue;padding:5px">
+  <h4 style="text-align:center">Contact Me</h4>
+</div>
+<!-- Navigation Bar -->
+<div class="navbar">
+  <a href="index.home">Home</a>
+    <a href="index.html">About</a>
+    <a href="Index.project.html">Projects</a>
+      <a href="Index.interest.html">Hobbies And Interests</a>
+      <a href="Index.skills.html">Key skills </a>
+    <a href="Index.contact.html">Contacts</a>
+  
+  </div>
+
+    <section id="Contacts">
+        
+	<img src="images (4).png"alt="Myself" width="200" height="200">
+        <P><b><mark>Contacts</mark></b></P> 
+
+          
+
+        <!--<div class="fakeimg" style="height:200px;">Image</div> -->
+        <ul>
+    <li>
+    <p>
+    Email: <a href="mailto:wendymagobiane001@gmail.com">wendymagobiane001@gmail.com</a>
+    </p>
+    </li>
+    <li>
+    <p> Phone:(078) 306 7623
+    </p>
+    <li>
+    <p>Whatsapp:<a href="https://wa.me/27783067623?text=I'm%20interested%20in%20your%20portfolio"</p>
+    <button style="font-size:24px"> <i class="fa fa-whatsapp"></i></button> </a>
+
+    </li>
+    <p>
+     <li>Facebook: <a target="_blank" title="follow me on facebook" href="https://www.facebook.com/profile.php?id=10000501676694 "><img alt="follow me on facebook" src="https://c866088.ssl.cf3.rackcdn.com/assets/facebook40x40.png" border=0></a>
+      </a></li>
+      </li>
+        </ul>
+    </section>
+    
+      <div class="container">
+        <div style="text-align:center">
+         <h2>Get In Touch</h2>
+          
+            
+  <div style="text-align:center">
+    
+  </div>
+   </div>
+   <div class="container">
+    <form action="action_page.php">
+  
+      <label for="fname">First Name</label>
+      <input type="text" id="fname" name="firstname" placeholder="Your name..">
+  
+      <label for="lname">Last Name</label>
+      <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+  
+     
+      <label for="subject">Subject</label>
+      <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+      <div class="row">
+        <div class="g-recaptcha" data-sitekey="6Lf1_u0ZAAAAAKvATuNlA5VaveBxqtRCrPcfmB7B"></div>   
+  
+      <input type="submit" value="Submit">
+      <script src='https://www.google.com/recaptcha/api.js'></script>
+  
+    </form>
+  </div>
+    
+
+    </p>
+    </ul> 
+    
+
+
+
+   
+      </div>
+    
+    </div>
+  </div>
+</div>
+<!-- Footer -->
+<div class="footer">
+	<centre><h4><p> &copy;2020-Lusanda-All rights reseved</p></h4></centre>
+  </div>
+     
+
+
+
+
+
+
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+
+    if (isset($_POST['name']) && isset($_POST['email'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $body = $_POST['body'];
+
+        require_once "PHPMailer/PHPMailer.php";
+        require_once "PHPMailer/SMTP.php";
+        require_once "PHPMailer/Exception.php";
+
+        $mail = new PHPMailer();
+
+        //SMTP Settings
+        $mail->isSMTP();
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = true;
+        $mail->Username = "wendymagobiane001@gmail.com";
+        $mail->Password = '0735155665';
+        $mail->Port = 465; //587
+        $mail->SMTPSecure = "ssl"; //tls
+
+        //Email Settings
+        $mail->isHTML(true);
+        $mail->setFrom($email, $name);
+        $mail->addAddress("wendymagobiane001@gmai@gmail.com");
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+
+        if ($mail->send()) {
+            $status = "success";
+            $response = "Email is sent!";
+        } else {
+            $status = "failed";
+            $response = "Something is wrong: <br><br>" . $mail->ErrorInfo;
+        }
+
+        exit(json_encode(array("status" => $status, "response" => $response)));
+    }
+?>
+
+</body>
+    </html> 
